11874f581e99240fe38f32677615dff933fb8e84
vadim-ivlev <vadim.ivlev@gmail.com>
2023-01-26 15:08:45 +0000

    add: stream handler.

---
+++ _requests/create-files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dir=$(dirname "$0")
+
+file=$dir/100M
+dd if=/dev/zero of=$file bs=1M count=100 status=progress
+file=$dir/1G
+dd if=/dev/zero of=$file bs=1M count=1024 status=progress
+file=$dir/5G
+dd if=/dev/zero of=$file bs=1M count=5170 status=progress
--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	engine := gin.Default()
+	engine.POST("/streamupload", streamuploadHandler)
+	if err := engine.Run("0.0.0.0:19090"); err != nil {
+		panic(err)
+	}
+}
+
+// streamuploadHandler is a handler for uploading a file.
+func streamuploadHandler(c *gin.Context) {
+	uploadedFileName := filepath.Join(getWorkingDir(), "file")
+
+	if c.GetHeader("Content-Type") != "application/octet-stream" {
+		err := fmt.Errorf("required octet-stream")
+		c.AbortWithStatusJSON(400, map[string]string{"message": err.Error()})
+		return
+	}
+	file, err := os.Create(uploadedFileName)
+	if err != nil {
+		c.AbortWithStatusJSON(400, gin.H{"message": "Create: " + err.Error()})
+		return
+	}
+	defer file.Close()
+	_, err = io.Copy(file, c.Request.Body)
+	if err != nil {
+		c.AbortWithStatusJSON(400, gin.H{"message": "Copy: " + err.Error()})
+		return
+	}
+	c.JSON(200, map[string]string{"message": "ok stream"})
+}
+
+// getWorkingDir returns the current working directory.
+func getWorkingDir() string {
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	return dir
 }
